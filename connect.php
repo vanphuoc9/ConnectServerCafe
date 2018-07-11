cc4f88e146221ce97478e79d32ba7b7efcbb5505
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-07-11 09:31:27 +0700

    Revert "first commit"
    This reverts commit b16a36a020d76c9f2e61254d97ba7a26be6ce73d.

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = "localhost";
-  $username = "root";
-  $password = "";
-  $database = "cafe";
-  $connect=mysqli_connect($host,$username,$password,$database);
-  mysqli_query($connect,"SET NAMES 'utf8'");
-
-?>
