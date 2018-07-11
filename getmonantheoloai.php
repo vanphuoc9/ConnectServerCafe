75619af7fbfa9a383bddea4dc5cd5214a78491b0
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-07-11 16:17:19 +0700

    danh sach mon theo loai

--- getmonantheoloai.php
+++ getmonantheoloai.php
@@ -4,35 +4,37 @@
   // phương thức GET bắt những giá trị trên thanh địa chỉ
   $page = $_GET['page'];
   $idloai = $_POST['idloai'];
+
+  // $page = 1;
+  // $idloai = 1;
   
   // biến này giới hạn giá trị dữ liệu về
-  $space = 6;
+  $space = 4;
     //  xác định vị trí đầu tiên
   // ví vụ lần đầu load từ 1->5 lần sau load từ 6->10
   $limit = ($page - 1) * $space;
-  class monan{
-  	function monan($id,$tenmon,$hinhmon,$motamon,$nguyenlieu,$cachlam,$idloai){
-  		$this->id = $id;
-  		$this->tenmon = $tenmon;
-  		$this->hinhmon = $hinhmon;
-  		$this->motamon = $motamon;
-  		$this->nguyenlieu = $nguyenlieu;
-  		$this->cachlam = $cachlam;
-  		$this->idloai = $idloai;
-  	}
+  class mon{
+    function mon($mamon,$maloai,$ten,$hinh,$mota,$dongia){
+      $this->mamon = $mamon;
+      $this->maloai = $maloai;
+      $this->ten = $ten;
+      $this->hinh = $hinh;
+      $this->mota = $mota;
+      $this->dongia = $dongia;
+    
+    }
   }
-  $query = "SELECT * FROM monan where idloai = $idloai LIMIT $limit,$space";
+  $query = "SELECT * FROM mon where maloai = $idloai LIMIT $limit,$space";
   $data = mysqli_query($connect,$query);
   $mang = array();
   while($row = mysqli_fetch_assoc($data)){
-      array_push($mang, new monan(
-            $row['id'],
-            $row['tenmon'],
-            $row['hinhmon'],
-            $row['motamon'],
-            $row['nguyenlieu'],
-            $row['cachlam'],
-            $row['idloai']  
+      array_push($mang, new mon(
+            $row['MAMON'],
+            $row['MALOAI'],
+            $row['TEN'],
+            $row['HINH'],
+            $row['MOTA'],
+            $row['DONGIA']  
       
       ));
   }
