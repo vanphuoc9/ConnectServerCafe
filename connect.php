153fe53de5e1e3ac48efe5ff7f714c41a24b17ab
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-07-17 15:45:21 +0700

    csdl

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
   $host = "localhost";
   $username = "root";
   $password = "";
-  $database = "cafe";
+  $database = "quanlycaffe";
   $connect=mysqli_connect($host,$username,$password,$database);
   mysqli_query($connect,"SET NAMES 'utf8'");
 
