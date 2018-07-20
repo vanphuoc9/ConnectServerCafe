518e10bf42cce398e5edb1e04575609f9400ccc7
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-07-20 13:24:19 +0700

    ds mon chua thanh toan theo ban

---
+++ getdsmchuatttheoban.php
@@ -0,0 +1,27 @@
+<?php
+  include "connect.php";
+  $sttban = $_POST['STTBAN'];
+  class giohang{
+    function giohang($mamon,$ten,$dongia,$hinh,$soluong){
+      $this->mamon = $mamon;
+      $this->ten = $ten;
+      $this->dongia = $dongia;
+      $this->hinh = $hinh;
+      $this->soluong = $soluong;
+    
+    }
+  }
+  $query = "SELECT `ct_dm`.mamon, mon.ten, mon.dongia, mon.hinh, `ct_dm`.soluong from `ct_dm` join mon on `ct_dm`.mamon = mon.mamon where trangthai = 0 and sttban = '$sttban';";
+  $data = mysqli_query($connect,$query);
+  $mang = array();
+  while($row = mysqli_fetch_assoc($data)){
+      array_push($mang, new giohang(
+            $row['mamon'],
+            $row['ten'],
+            $row['dongia'],
+            $row['hinh'],
+            $row['soluong']  
+      ));
+  }
+  echo json_encode($mang);
+?>
